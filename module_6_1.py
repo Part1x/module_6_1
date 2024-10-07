85a5a5e83c009b6baf0c422fb1181691626b3a0a
Part1x <part1xtube@gmail.com>
2024-10-07 17:27:35 +0300

    Add files via upload

---
+++ module_6_1.py
@@ -0,0 +1,74 @@
+class Animal:
+    alive = True
+    fed = False
+
+    def __init__(self, name):
+        self.name = name
+
+
+class Plant:
+    edible = False
+
+    def __init__(self, name):
+        self.name = name
+
+
+class Mammal(Animal):
+    def __init__(self, name):
+        super().__init__(name)
+        self.alive = True
+        self.fed = False
+
+    def eat(self, food):
+        if food.edible == False:
+            print(f"{self.name} не стал есть {food.name}")
+            self.alive = False
+        else:
+            print(f"{self.name} съел {food.name}")
+            self.fed = True
+
+
+class Predator(Animal):
+    edible = False
+    def __init__(self, name):
+        super().__init__(name)
+        self.alive = True
+        self.fed = False
+
+    def eat(self, food):
+
+        if food.edible == True:
+            print(f"{self.name} съел {food.name}")
+            self.fed = True
+        else:
+            print(f"{self.name} не стал есть {food.name}")
+            self.alive = False
+
+
+class Flower(Plant):
+    def __init__(self, name):
+        super().__init__(name)
+
+
+class Fruit(Plant):
+    edible = True
+
+    def __init__(self, name):
+        super().__init__(name)
+
+
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+
+print(a1.name)
+print(p1.name)
+
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
+
